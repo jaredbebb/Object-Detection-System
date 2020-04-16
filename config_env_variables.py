241993adc4c8505184225cca60fb6d12e83f7f2f
Jared Bebb <j.bebb@live.com>
2020-04-16 12:30:43 -0500

    built directory to resolve FileNotFoundError: [Errno 2] No such file or directory: '/home/travis/build/jaredbebb/Object-Detection-System/temp_lib/config/'

--- config_env_variables.py
+++ config_env_variables.py
@@ -6,11 +6,11 @@ import shutil
 
 print("using python version:",sys.version)
 
-ROOT = os.getcwd()
+current_dir = os.getcwd()
 
 def copy_dir():
-	shutil.copy(ROOT+"/extra/env_config.cpp", ROOT+"/temp_lib/config/")
-	shutil.copy(ROOT+"/extra/env_config.h", ROOT+"/temp_lib/config/")
+	shutil.copy(current_dir+"/extra/env_config.cpp", current_dir+"/temp_lib/config/")
+	shutil.copy(current_dir+"/extra/env_config.h", current_dir+"/temp_lib/config/")
 
 def replace_tokens(file,searchExp,replaceExp):
     print("file:"+file)
@@ -26,26 +26,26 @@ def construct_file_if_not_exists(original,target):
         shutil.copyfile(src=original,dst=target)
 
 #create /conf/config.json for testing
-construct_file_if_not_exists(ROOT+"/conf/config_demo.json",ROOT+"/conf/config.json")
+construct_file_if_not_exists(current_dir+"/conf/config_demo.json",current_dir+"/conf/config.json")
 
 # change permissions to rwx
 os.umask(0o777)
 
 # create temp_lib/config/ dir
-temp_lib_config_path = os.path.join(ROOT,"temp_lib/")
-print("",temp_lib_config_path)
-os.makedirs(name=temp_lib_config_path, exist_ok=True)
+temp_lib_config_path = os.path.join(current_dir,"temp_lib/")
+print("temp_lib_config_path",temp_lib_config_path)
+os.mkdir(path=temp_lib_config_path,mode=0o777)
 
 temp_lib_config_path = os.path.join(temp_lib_config_path,"config/")
-print("",temp_lib_config_path)
-os.makedirs(name=temp_lib_config_path, exist_ok=True)
+print("temp_lib_config_path",temp_lib_config_path)
+os.makedirs(name=temp_lib_config_path,mode=0o777, exist_ok=True)
 
 json_config_path = os.getcwd()+"/conf/config.json"
 print("json_config_path:"+json_config_path)
 with open(json_config_path) as f:
     data= json.load(f)
 
-cpp_file = ROOT+"/temp_lib/config/env_config.cpp"
+cpp_file = current_dir+"/temp_lib/config/env_config.cpp"
 match= "{$env_variables}"
 
 copy_dir()
