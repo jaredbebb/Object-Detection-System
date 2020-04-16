609fea0d7347d4a2457ce3a5054b3867dc1ca2d4
Jared Bebb <j.bebb@live.com>
2020-04-16 14:14:34 -0500

    split platformio configuration script

--- copy_source_files.py
+++ copy_source_files.py
@@ -25,20 +25,18 @@ construct_file_if_not_exists(current_dir+"/conf/config_demo.json",current_dir+"/
 
 oldmask = os.umask(0o000)
 print("oldmask",oldmask)
-oldmask1 = os.umask(0o777)
-print("oldmask1",oldmask1)
 
 
 # create temp_lib/config/ dir
 temp_lib_config_path = os.path.join(current_dir,"temp_lib/")
 print("temp_lib_config_path",temp_lib_config_path)
-os.mkdir(path=temp_lib_config_path,mode=0o777)
+os.mkdir(path=temp_lib_config_path)
 
 temp_lib_config_path = os.path.join(current_dir,"temp_lib/config/")
 print("temp_lib_config_path",temp_lib_config_path)
-os.mkdir(path=temp_lib_config_path,mode=0o777)
+os.mkdir(path=temp_lib_config_path)
 
 copy_dir()
 
-oldmask2 = os.umask(oldmask)
-print("oldmask2",oldmask2)
+oldmask = os.umask(oldmask)
+print("oldmask",oldmask)
